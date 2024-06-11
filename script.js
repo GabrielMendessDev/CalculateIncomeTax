93d3146a3f0be882bb61948f99f94b6853d92d80
Gabriel Mendes <lucasgabriel16161@gmail.com>
2024-06-11 13:37:29 -0300

    mudando caminho das imagens, pois estava dando erro

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ function updateCurrencyImage() {
     const currencyImage = document.getElementById('currency-image');
     switch(currency) {
         case 'real':
-            currencyImage.src = '/assets/real.svg';
+            currencyImage.src = '/assets/real.png';
             break;
         case 'dolar':
-            currencyImage.src = '/assets/dolar.svg';
+            currencyImage.src = '/assets/dolar.png';
             break;
         case 'euro':
-            currencyImage.src = '/assets/euro.svg';
+            currencyImage.src = '/assets/euro.png';
             break;
         default:
             currencyImage.src = '';
