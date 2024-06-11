4aa66d91f971c129df2de62e0a828a4a9b6cd060
Gabriel Mendes <lucasgabriel16161@gmail.com>
2024-06-11 15:58:16 -0300

    adicionando validações nos inputs

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function calculateTax() {
     const salary = parseFloat(document.getElementById('salary').value);
+    const inputSalary = document.getElementById('salary');
     const currency = document.getElementById('currency').value;
+    const result = document.getElementById('result');
     let taxRate;
 
     switch(currency) {
@@ -16,27 +18,41 @@ function calculateTax() {
     }
 
     const tax = salary * taxRate;
-    if(currency == 'real'){
-        document.getElementById('result').innerText = `O imposto total é de: R$ ${tax.toFixed(2)} reais`;
-    }else if(currency == 'dolar'){
-        document.getElementById('result').innerText = `O imposto total é de: $ ${tax.toFixed(2)} doláres`;
+    if(salary && salary > 0 && currency != 'default'){
+        if(currency == 'real'){
+            result.innerText = `O imposto total é de: R$ ${tax.toFixed(2)} reais`;
+        }else if(currency == 'dolar'){
+            result.innerText = `O imposto total é de: $ ${tax.toFixed(2)} doláres`;
+        }else{
+            result.innerText = `O imposto total é de: € ${tax.toFixed(2)} euros`;
+        }
     }else{
-        document.getElementById('result').innerText = `O imposto total é de: € ${tax.toFixed(2)} euros`;
+        result.innerText = `⚠️ Erro ao calcular imposto. Preencha todos os campos. O salário não pode ser negativo.`;
+        result.style.color = "white";
+        inputSalary.focus();
     }
+
 }
 
 function updateCurrencyImage() {
     const currency = document.getElementById('currency').value;
     const currencyImage = document.getElementById('currency-image');
     switch(currency) {
+        case 'default':
+            currencyImage.src = './assets/interrogation.png';
+            result.innerText = ``;
+            break;
         case 'real':
             currencyImage.src = './assets/real.png';
+            result.innerText = ``;
             break;
         case 'dolar':
             currencyImage.src = './assets/dolar.png';
+            result.innerText = ``;
             break;
         case 'euro':
             currencyImage.src = './assets/euro.png';
+            result.innerText = ``;
             break;
         default:
             currencyImage.src = '';
